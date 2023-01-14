9269370703b31610e4c6c867bd16589a50c33400
Kaushal <Kausraj11@gmail.com>
2023-01-14 11:41:30 +0530

    Changes made

--- main.js
+++ main.js
@@ -13,16 +13,13 @@ let pressure = document.querySelector("#pressure");
 let maxtemp = document.querySelector("#maxtemp");
 let mintemp = document.querySelector("#mintemp");
 let feelslike = document.querySelector("#feelslike");
-let cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, maxtempvar, mintempvar, feelslikevar;
-let city;
+let city,cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, maxtempvar, mintempvar, feelslikevar;
 
 function tempConverter(kelvin) {
-    return Math.round(kelvin - 273.15)
+    return Math.round(kelvin - 273.15);
 }
-function unixToDateConverter(unixDate){
+function unixToDateConverter(unixDate) {
     let date = new Date(unixDate * 1000);
-    // let result = date.slice(0, 10);
-    // console.log(result);
     return date;
 }
 function unixToTimeConverter(unixDate) {
@@ -31,28 +28,27 @@ function unixToTimeConverter(unixDate) {
     let minutes = "0" + date.getMinutes();
     let seconds = "0" + date.getSeconds();
     let formatTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return(formatTime);
+    return (formatTime);
 }
 submitBtn.addEventListener("click", () => {
     city = textValue.value;
-    console.log(city);
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3045dd712ffe6e702e3245525ac7fa38`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
             cityname = data['name'];
             descvar = data['weather']['0']['description'];
-            tempvar = data['main']['temp']
-            windvar = data['wind']['speed']
-            visibilityvar = data['visibility']
-            datevar = data['dt']
-            sunrisevar = data['sys']['sunrise']
-            sunsetvar = data['sys']['sunset']
-            pressurevar = data['main']['pressure']
-            maxtempvar = data['main']['temp_max']
-            mintempvar = data['main']['temp_min']
-            humidityvar = data['main']['humidity']
-            feelslikevar = data['main']['feels_like']
+            tempvar = data['main']['temp'];
+            windvar = data['wind']['speed'];
+            visibilityvar = data['visibility'];
+            datevar = data['dt'];
+            sunrisevar = data['sys']['sunrise'];
+            sunsetvar = data['sys']['sunset'];
+            pressurevar = data['main']['pressure'];
+            maxtempvar = data['main']['temp_max'];
+            mintempvar = data['main']['temp_min'];
+            humidityvar = data['main']['humidity'];
+            feelslikevar = data['main']['feels_like'];
             cityoutput.innerHTML = `Weather of ${cityname}`;
             description.innerHTML = `Description :  ${descvar}`;
             temp.innerHTML = `Temperature : ${tempConverter(tempvar)}°C`;
