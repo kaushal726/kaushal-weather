4a5d71b2032907f0f74e436d1969ce903d9eba2b
Kaushal <Kausraj11@gmail.com>
2023-01-14 11:57:48 +0530

    Changes made

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let pressure = document.querySelector("#pressure");
 let maxtemp = document.querySelector("#maxtemp");
 let mintemp = document.querySelector("#mintemp");
 let feelslike = document.querySelector("#feelslike");
-let city,cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, maxtempvar, mintempvar, feelslikevar;
+let city, cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, maxtempvar, mintempvar, feelslikevar;
 
 function tempConverter(kelvin) {
     return Math.round(kelvin - 273.15);
@@ -35,7 +35,6 @@ submitBtn.addEventListener("click", () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3045dd712ffe6e702e3245525ac7fa38`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             cityname = data['name'];
             descvar = data['weather']['0']['description'];
             tempvar = data['main']['temp'];
