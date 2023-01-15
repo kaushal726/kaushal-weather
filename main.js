75cc8d909831d177b41b07091b37a4d1d0542ed3
Kaushal <Kausraj11@gmail.com>
2023-01-15 15:00:16 +0530

    Changes made

--- main.js
+++ main.js
@@ -10,28 +10,42 @@ let sunrise = document.querySelector("#sunrise");
 let sunset = document.querySelector("#sunset");
 let humidity = document.querySelector("#humidity");
 let pressure = document.querySelector("#pressure");
-let maxtemp = document.querySelector("#maxtemp");
-let mintemp = document.querySelector("#mintemp");
 let feelslike = document.querySelector("#feelslike");
-let city, cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, maxtempvar, mintempvar, feelslikevar;
+let city, cityname, descvar, tempvar, windvar, visibilityvar, datevar, sunrisevar, sunsetvar, humidityvar, pressurevar, feelslikevar;
 
 function tempConverter(kelvin) {
     return Math.round(kelvin - 273.15);
 }
 function unixToDateConverter(unixDate) {
     let date = new Date(unixDate * 1000);
+    // let newDatw = date.substring(1, 10);
+    console.log(date);
     return date;
 }
-function unixToTimeConverter(unixDate) {
-    let date = new Date(unixDate * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let seconds = "0" + date.getSeconds();
-    let formatTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    return (formatTime);
+
+function unixToTimeConverter(unixDate, which) {
+    if (which != "sunset") {
+        let date = new Date(unixDate * 1000);
+        let hours = date.getHours();
+        let minutes = "0" + date.getMinutes();
+        let seconds = "0" + date.getSeconds();
+        let formatTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return (formatTime);
+    }
+    else {
+        let date = new Date(unixDate * 1000);
+        let hours = date.getHours();
+        hours = hours - 12;
+        let minutes = "0" + date.getMinutes();
+        let seconds = "0" + date.getSeconds();
+        let formatTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return (formatTime);
+    }
 }
+
 submitBtn.addEventListener("click", () => {
     city = textValue.value;
+
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=59467ea71641323dee7468c3b76b7ecc`)
         .then(res => res.json())
         .then(data => {
@@ -44,23 +58,21 @@ submitBtn.addEventListener("click", () => {
             sunrisevar = data['sys']['sunrise'];
             sunsetvar = data['sys']['sunset'];
             pressurevar = data['main']['pressure'];
-            maxtempvar = data['main']['temp_max'];
-            mintempvar = data['main']['temp_min'];
             humidityvar = data['main']['humidity'];
             feelslikevar = data['main']['feels_like'];
             cityoutput.innerHTML = `Weather of ${cityname}`;
             description.innerHTML = `Description :  ${descvar}`;
-            temp.innerHTML = `Temperature : ${tempConverter(tempvar)}°C`;
-            wind.innerHTML = `Wind :  ${windvar}`;
+            temp.innerHTML = `Temperature : ${tempConverter(tempvar)} °C`;
+            wind.innerHTML = `Wind :  ${windvar} mph`;
             date.innerHTML = `Date : ${unixToDateConverter(datevar)}`;
-            sunrise.innerHTML = `Sunrise :  ${unixToTimeConverter(sunrisevar)}`;
-            sunset.innerHTML = `Sunset : ${unixToTimeConverter(sunsetvar)}`;
-            humidity.innerHTML = `Humidity : ${humidityvar}`;
-            pressure.innerHTML = `Pressure : ${pressurevar}`;
-            maxtemp.innerHTML = `Maximum Temperature : ${tempConverter(maxtempvar)}°C`;
-            mintemp.innerHTML = `Minimum Temperature : ${tempConverter(mintempvar)}°C`;
-            feelslike.innerHTML = `Feelslike :  ${tempConverter(feelslikevar)}°C`;
+            sunrise.innerHTML = `Sunrise :  ${unixToTimeConverter(sunrisevar, "sunrise")} am`;
+            sunset.innerHTML = `Sunset : ${unixToTimeConverter(sunsetvar, "sunset")} pm`;
+            humidity.innerHTML = `Humidity : ${humidityvar} %`;
+            pressure.innerHTML = `Pressure : ↑${pressurevar} mb`;
+            feelslike.innerHTML = `Feelslike :  ${tempConverter(feelslikevar)} °C`;
+            textValue.innerHTML = "";
         })
+
 })
 
 
